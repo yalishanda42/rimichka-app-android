60f9ebef8f030779b7ef91bfa2b993ac908b1170
Alexander Ignatov <ani1802@abv.bg>
2019-12-12 01:38:25 +0200

    Add search rhymes basic layout and search stub

--- app/src/main/java/bg/abv/ani1802/rimichka/search/SearchRhymesFragment.kt
+++ app/src/main/java/bg/abv/ani1802/rimichka/search/SearchRhymesFragment.kt
@@ -2,10 +2,14 @@ package bg.abv.ani1802.rimichka.search
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 import bg.abv.ani1802.rimichka.R
 
@@ -16,6 +20,15 @@ class SearchRhymesFragment : Fragment() {
     }
 
     private lateinit var viewModel: SearchRhymesViewModel
+    private lateinit var searchBar: EditText
+    private lateinit var searchButton: Button
+
+    private var lastSearchedWord: String? = null
+    private var searchButtonIsHidden: Boolean = true
+        set(value: Boolean) {
+            field = value
+            searchButton.visibility = if (value) View.GONE else View.VISIBLE
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +37,44 @@ class SearchRhymesFragment : Fragment() {
         return inflater.inflate(R.layout.search_rhymes_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchBar = view.findViewById(R.id.search_edit_text)
+        searchButton = view.findViewById(R.id.search_button)
+
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun onTextChanged(newSearchedWord: CharSequence?, start: Int, before: Int, count: Int) {
+                newSearchedWord?.let { newWord ->
+                    lastSearchedWord?.let { oldWord ->
+                        if (oldWord == newWord) {
+                            searchButtonIsHidden = true
+                        }
+                    } ?: run {
+                        if (newSearchedWord != "" && searchButtonIsHidden) {
+                            searchButtonIsHidden = false
+                        }
+                    }
+                }
+            }
+        })
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SearchRhymesViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    fun searchWordForRhymes(view: View) {
+        lastSearchedWord = searchBar.text.toString()
+        // TODO: Ask the ViewModel to fetch rhymes
+        // TODO: Update UI
     }
 
 }
