32a367e62425021060cd795a3fadf6cf4b44826c
Aleksandar Ignatov <ani1802@abv.bg>
2019-12-11 20:31:22 +0200

    Initial commit (empty Bottom navigation project)

---
+++ app/src/main/java/bg/abv/ani1802/rimichka/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package bg.abv.ani1802.rimichka.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
